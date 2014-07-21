5f4dcfd1b263a3c2a2f0db9c22e2440cc6b2a8c7
DLE.NET.TR <dlenettr@users.noreply.github.com>
2014-07-21 16:57:39 +0300

    Genel düzenleme yapıldı

--- engine/modules/simplebb.php
+++ engine/modules/simplebb.php
@@ -8,8 +8,6 @@
  Mail: m.hanoglu55@gmail.com
 -----------------------------------------------------
  Lisans : GPL License
------------------------------------------------------
- Tarih : 15.07.2014
 =====================================================
 */
 
@@ -17,41 +15,65 @@ if( ! defined( 'DATALIFEENGINE' ) ) {
 	die( "Hacking attempt!" );
 }
 
-$tpl->result['main'] = str_replace ( "{category-name}", $cat_info[ $category_id ]['name'], $tpl->result['main'] );
-$tpl->result['main'] = str_replace ( "{category-id}", $category_id, $tpl->result['main'] );
-if ( $config['forum_use_subdomain'] AND ! empty( $cat_info[ $config['forum_id'] ]['alt_name'] ) ) {
-	$main_host = str_replace( $cat_info[ $config['forum_id'] ]['alt_name'] . ".", "", $_SERVER['HTTP_HOST'] );
-	$tpl->result['main'] = str_replace ( "http://www." . $main_host . "/" . $cat_info[ $config['forum_id'] ]['alt_name'], "http://" . $cat_info[ $config['forum_id'] ]['alt_name'] . "." . $main_host, $tpl->result['main'] );
-	$tpl->result['main'] = str_replace ( "http://" . $main_host . "/" . $cat_info[ $config['forum_id'] ]['alt_name'], "http://" . $cat_info[ $config['forum_id'] ]['alt_name'] . "." . $main_host, $tpl->result['main'] );
-}
 
-if ( strpos( $tpl->result['main'], "{forum-stats}" ) !== false ) {
-	if ( ! isset( $forum ) ) {
-		include_once (ENGINE_DIR . '/modules/show.forum.php');
-		$forum = new SimpleBB( $config, $db, $tpl, $cat_info, $user_groups, $member_id );
-	}
-	$tpl->result['main'] = str_replace( "{forum-stats}", $forum->get_stats(), $tpl->result['main'] );
-}
+if ( ! in_array( $dle_module, array("addnews") ) ) {
 
-if ( stripos( $tpl->result['main'], "[forum" ) !== false ) {
-	if ( $forum_where == "forum" OR $forum_where == "cat" ) {
-		foreach ( $banners as $name => $value ) {
-			$tpl->result['main'] = str_replace ( "{banner_" . $name . "}", $value, $tpl->result['main'] );
-			if ( $value ) {
-				$tpl->result['main'] = str_replace ( "[banner_" . $name . "]", "", $tpl->result['main'] );
-				$tpl->result['main'] = str_replace ( "[/banner_" . $name . "]", "", $tpl->result['main'] );
+	if ( $dle_module == "main" ) $forum_where = "";
+
+	if ( $forum_compile == "before" ) {
+
+		// Category Echo - start
+		if (stripos ( $tpl->copy_template, "{category" ) !== false) {
+			$tpl->copy_template = preg_replace_callback ( "#\\{category(.+?)\\}#i", "custom_cat_print", $tpl->copy_template );
+		}
+		// Category Echo - end
+
+		if ( in_array( $forum_where, array( "cat", "forum", "thread" ) ) ) {
+
+			$forum_main_tpl = file_get_contents( ROOT_DIR . "/templates/" . $config['skin'] . "/forum/main.tpl" );
+			$tpl->result['content'] = str_replace( "{content}", $tpl->result['content'], $forum_main_tpl );
+
+		}
+
+	} else if ( $forum_compile == "after" ) {
+
+		if ( $config['forum_use_subdomain'] AND ! empty( $cat_info[ $config['forum_id'] ]['alt_name'] ) ) {
+			$main_host = str_replace( $cat_info[ $config['forum_id'] ]['alt_name'] . ".", "", $_SERVER['HTTP_HOST'] );
+			$tpl->result['main'] = str_replace ( "http://www." . $main_host . "/" . $cat_info[ $config['forum_id'] ]['alt_name'], "http://" . $cat_info[ $config['forum_id'] ]['alt_name'] . "." . $main_host, $tpl->result['main'] );
+			$tpl->result['main'] = str_replace ( "http://" . $main_host . "/" . $cat_info[ $config['forum_id'] ]['alt_name'], "http://" . $cat_info[ $config['forum_id'] ]['alt_name'] . "." . $main_host, $tpl->result['main'] );
+		}
+
+		if ( strpos( $tpl->result['main'], "{forum-stats}" ) !== false ) {
+			if ( ! isset( $forum ) ) {
+				include_once (ENGINE_DIR . '/modules/show.forum.php');
+				$forum = new SimpleBB( $config, $db, $tpl, $cat_info, $user_groups, $member_id );
 			}
+			$tpl->result['main'] = str_replace( "{forum-stats}", $forum->get_stats(), $tpl->result['main'] );
 		}
-		if ( stripos( $tpl->result['main'], "{custom" ) !== false) {
-			$tpl->result['main'] = preg_replace_callback ( "#\\{custom(.+?)\\}#i", "custom_print", $tpl->result['main'] );
+
+		if ( stripos( $tpl->result['main'], "[forum" ) !== false ) {
+			if ( $forum_where == "forum" OR $forum_where == "cat" ) {
+				foreach ( $banners as $name => $value ) {
+					$tpl->result['main'] = str_replace ( "{banner_" . $name . "}", $value, $tpl->result['main'] );
+					if ( $value ) {
+						$tpl->result['main'] = str_replace ( "[banner_" . $name . "]", "", $tpl->result['main'] );
+						$tpl->result['main'] = str_replace ( "[/banner_" . $name . "]", "", $tpl->result['main'] );
+					}
+				}
+				if ( stripos( $tpl->result['main'], "{custom" ) !== false) {
+					$tpl->result['main'] = preg_replace_callback ( "#\\{custom(.+?)\\}#i", "custom_print", $tpl->result['main'] );
+				}
+				$tpl->result['main'] = preg_replace( "#\\[forum:inside\\](.*?)\\[/forum:inside\\]#is", "$1", $tpl->result['main'] );
+			}
+			if ( ! empty( $forum_where ) && $forum_where != "inside" ) {
+				$tpl->result['main'] = preg_replace( "#\\[forum:" . $forum_where . "\\](.*?)\\[/forum:" . $forum_where . "\\]#is", "$1", $tpl->result['main'] );
+			}
+			$tpl->result['main'] = preg_replace( "#(\\[forum:([a-z]+)\\](.*?)\\[/forum:([a-z]+)\\])#is", "", $tpl->result['main'] );
+			$tpl->result['main'] = preg_replace( "#(\\[/forum:([a-z]+)\\])#is", "", $tpl->result['main'] );
 		}
-		$tpl->result['main'] = preg_replace( "#\\[forum:inside\\](.*?)\\[/forum:inside\\]#is", "$1", $tpl->result['main'] );
+
 	}
-	if ( ! empty( $forum_where ) && $forum_where != "inside" ) {
-		$tpl->result['main'] = preg_replace( "#\\[forum:" . $forum_where . "\\](.*?)\\[/forum:" . $forum_where . "\\]#is", "$1", $tpl->result['main'] );
-	}
-	$tpl->result['main'] = preg_replace( "#(\\[forum:([a-z]+)\\](.*?)\\[/forum:([a-z]+)\\])#is", "", $tpl->result['main'] );
-	$tpl->result['main'] = preg_replace( "#(\\[/forum:([a-z]+)\\])#is", "", $tpl->result['main'] );
+
 }
 
 ?>
